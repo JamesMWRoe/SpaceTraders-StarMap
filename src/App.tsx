bbb6915c1de8dae8ed76bc48aced329ef9439d68
JamesMWRoe <96987618+JamesMWRoe@users.noreply.github.com>
2024-10-22 18:57:36 +0100

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './styles/App.css'
+import NewGame from './Components/NewGame'
+
+function App() {
+
+
+  return (
+    <>
+      <NewGame />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/NewGame.tsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+
+export default function NewGame()
+{
+  const [ symbol, setSymbol ] = useState("");
+  const [ faction, setFaction ] = useState("COSMIC");
+
+  const [ resp, setResp ] = useState("");
+
+  return(  
+    <>
+      <div className="inputField">
+        <label htmlFor="userSymbol">Call Sign: </label>
+        <input type="text" id="userSymbol" value={symbol} onChange={(e) => {setSymbol(e.target.value)}}/>
+      </div>
+
+      <div className="inputField">
+        <label htmlFor="faction">Starting Faction</label>
+        <input type="text" id="faction"  value={faction} onChange={(e) => {setFaction(e.target.value)}} />
+      </div>
+
+      <button type="submit" onClick={Signup}>Sign Up</button>
+
+      <span>{resp}</span>
+    </>
+  )
+
+  async function Signup()
+  {
+    console.log(`symbol: ` + symbol);
+    console.log(`faction: ` + faction);
+
+    const resp = await fetch('https://api.spacetraders.io/v2/register', {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({
+        symbol: symbol,
+        faction: faction
+      })
+    });
+
+    const json = await resp.json();
+
+    if (resp.ok)
+    {
+      console.log("response OK!")
+    }
+
+    setResp(JSON.stringify(json, null, 2));
+
+  }
+}
+
