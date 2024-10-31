53845bea0fc5d5053ac8e5ae2d6683dcbf1e98e8
JamesMWRoe <96987618+JamesMWRoe@users.noreply.github.com>
2024-10-31 14:39:41 +0000

    Added Basic SideMenu

---
+++ src/Components/SideMenu.tsx
@@ -0,0 +1,16 @@
+export default function SideMenu()
+{
+  return(
+  <>
+    <h2 id="menuHead">Menu</h2>
+    <nav id="profileNav">
+      <ul>
+        <li className="sideMenuOption">Ships</li>
+        <li className="sideMenuOption">Contracts</li>
+        
+      </ul>
+    </nav>
+  </>
+    
+  )
+}
