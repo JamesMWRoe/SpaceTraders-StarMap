bd5a952c1b1fddd79506df38c2007d8393e79596
JamesMWRoe <96987618+JamesMWRoe@users.noreply.github.com>
2024-11-01 18:38:13 +0000

    merge conflict definitely resolved

--- src/Components/Game.tsx
+++ src/Components/Game.tsx
@@ -1,29 +1,16 @@
-<<<<<<< HEAD
-import { useAgentContext } from "../Context/AgentContext"
-import ContractMenu from "./ContractMenu";
-import ShipMenu from "./ShipMenu";
-=======
 import StarMap from "./StarMap";
 import WaypointContextMenu from "./WaypointContextMenu";
 import '../styles/Game.css'
 import SelectedWaypointContextProvider from "../Context/SelectedWaypointContext";
->>>>>>> master
 
 export default function Game()
 {
   return(
     <div id="mainGame">
-<<<<<<< HEAD
-      <h1>Main Game</h1>
-      
-      <ShipMenu />
-
-=======
       <SelectedWaypointContextProvider>
         <StarMap />
         <WaypointContextMenu />
       </SelectedWaypointContextProvider>
->>>>>>> master
     </div>
   )
 }
